b1fec0b8d3ad01632dffadea4f405fd66e3e5747
couribel <42025151+couribel@users.noreply.github.com>
2023-10-19 15:01:45 +0200

    Update pedantix.js

--- pedantix.js
+++ pedantix.js
@@ -1,25 +1,25 @@
 // Pour gagner du temps au jeu Pedantix : https://cemantix.certitudes.org/pedantix
 // Source : https://www.reddit.com/r/cemantix/comments/vjjpps/script_pedantix/?rdt=64067
-function traiter_liste(liste_mots) {
-    $("#pedantix-guess").value = liste_mots[0];
+function traiter_liste(Liste) {
+    $("#pedantix-guess").value = Liste[0];
     $('#pedantix-guess-btn').click();
-    liste_mots.splice(0,1);
-    if (liste_mots.length > 0) {
-        setTimeout(function(){ traiter_liste(liste_mots) }, TIMEOUT);
+    Liste.splice(0,1);
+    if (Liste.length > 0) {
+        setTimeout(function(){ traiter_liste(Liste) }, TIMEOUT);
     }
 }
-function traiter_liste2(liste_mots) {
+function traiter_liste2(Liste) {
   ii = 0;
   while (ii<nbmots) {
-    $("#pedantix-guess").value = liste_mots[ii];
+    $("#pedantix-guess").value = Liste[ii];
     $('#pedantix-guess-btn').click();
-//    console.log(liste_mots[ii]);
+//    console.log(Liste[ii]);
 // Pas possible de faire une simple "pause" ==> setTimeout(...)
     ii++;
   }
 }
-nbmots = liste_mots.length;
-//	console.log('Nb mots : ' + nbmots);
-liste_mots = ['le', 'de', 'du', 'au', 'à', 'que', 'qui', 'il', 'elle', 'on', 'être', 'avoir', 'homme', 'femme', 'un', 'une', 'ce', 'se', 'à', 'ou', 'où', 'et', 'au', 'alors', 'afin', 'pour', 'or', 'donc', 'faire', 'pouvoir', 'son','ça', 'ce', 'ceci', 'cela', 'tout', 'toute', 'tous', 'tour', 's', 't', 'n', 'y', 'c', 'l', 'avant', 'après', 'e', 'siècle', 'animal', 'insecte', 'sport', 'art', 'science', 'physique', 'en', 'ne', 'pas', 'par', 'pour', 'car', 'cas', 'dans', 'avec', 'sans', 'plus', 'moins', 'tout', 'guerre', 'faut', 'pays', 'ville', 'langue', 'exemple', 'début', 'fin', 'forme', 'temps', 'date', 'durée', 'année', 'famille', 'oiseau', 'période', 'grand', 'petit', 'habitant', 'population', 'nombre', 'certain', 'espèce', 'taille', 'chaud', 'froid', 'né', 'mort', 'arbre', 'feuille', 'végétaux', 'nord', 'sud', 'ouest', 'fleur', 'arbre', 'mesure', 'monde', 'origine'];
+let liste_mots = ['le', 'de', 'du', 'au', 'à', 'que', 'qui', 'il', 'elle', 'on', 'être', 'avoir', 'homme', 'femme', 'un', 'une', 'ce', 'se', 'à', 'ou', 'où', 'et', 'au', 'alors', 'afin', 'pour', 'or', 'donc', 'faire', 'pouvoir', 'son','ça', 'ce', 'ceci', 'cela', 'tout', 'toute', 'tous', 'tour', 's', 't', 'n', 'y', 'c', 'l', 'avant', 'après', 'e', 'siècle', 'animal', 'insecte', 'sport', 'art', 'science', 'physique', 'en', 'ne', 'pas', 'par', 'pour', 'car', 'cas', 'dans', 'avec', 'sans', 'plus', 'moins', 'tout', 'guerre', 'faut', 'pays', 'ville', 'langue', 'exemple', 'début', 'fin', 'forme', 'temps', 'date', 'durée', 'année', 'famille', 'oiseau', 'période', 'grand', 'petit', 'habitant', 'population', 'nombre', 'certain', 'espèce', 'taille', 'chaud', 'froid', 'né', 'mort', 'arbre', 'feuille', 'végétaux', 'nord', 'sud', 'ouest', 'fleur', 'arbre', 'mesure', 'monde', 'origine'];
 var TIMEOUT = 200;
+var nbmots = liste_mots.length;
+//	console.log('Nb mots : ' + nbmots);
 traiter_liste(liste_mots);
